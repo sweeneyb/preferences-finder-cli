d7e5fd8ad4223881cbf3cd500636c61ad06f1f0f
sweeneyb <tab126@gmail.com>
2021-06-09 09:18:07 -0400

    pulls data w/ some format options; arg 1 is the user

--- pullData.js
+++ pullData.js
@@ -1,5 +1,11 @@
 var admin = require('firebase-admin');
 
+
+let user = "guid"
+if(process.argv.length > 2) {
+  user = process.argv[2]
+}
+
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: 'https://preference-finder.firebaseio.com'
@@ -7,12 +13,27 @@ admin.initializeApp({
 
 const collectionsRef = admin.firestore().collection("ratings")
 
-collectionsRef.where("user", "==", "guid").get().then(query => {
+collectionsRef.where("user", "==", user)
+  .orderBy('timestamp', 'asc').get().then(query => {
     if (query.empty) {
         console.log("no results")
     } else {
         query.docs.forEach(doc => {
-            console.log(JSON.stringify(doc.data()))
+            let item = getSummary(doc.data())
+            console.log(JSON.stringify(item))
+        })
+
+        console.log("")
+        console.log("item, rating, time")
+        query.docs.forEach(doc => {
+            let item = getSummary(doc.data())
+            let array = Object.values(item).join(",");
+            console.log(array)
         })
     }
 })
+
+
+function getSummary(document) {
+        return { image: document.image, rating: document.rating, time: document.timestamp.toDate()}
+}
